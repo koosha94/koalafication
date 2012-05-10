f474b0c8c7efa7b7c1ded68fd63bcb128f5f3abe
koosha <koosha.mirhosseini@gmail.com>
2012-05-10 21:57:52 +0430

    Inspector

---
+++ src/main/java/chimp/ui/Inspector/Inspector.java
@@ -0,0 +1,19 @@
+package chimp.ui.Inspector;
+
+import javax.swing.*;
+
+/**
+ * @author koosha
+ * @since 1.0 (5/10/12, 9:43 PM)
+ */
+public class Inspector {
+    private JPanel Value;
+    private JPanel Action;
+    private JTabbedPane Tabs;
+    private JPanel MainPane;
+    private JTree tree1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JList list1;
+    private JButton button1;
+}
